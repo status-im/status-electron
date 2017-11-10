2c240d2b8b86e206f76e09da1da0cb6d614201d6
Andrey Shovkoplyas <motor4ik@gmail.com>
2017-11-10 14:51:31 -0500

    externs

---
+++ app/prod/js/externs.js
@@ -0,0 +1,14 @@
+var TopLevel = {
+"app" : function () {},
+"BrowserWindow" : function () {},
+"console" : function () {},
+"crashReporter" : function () {},
+"loadURL" : function () {},
+"log" : function () {},
+"on" : function () {},
+"platform" : function () {},
+"quit" : function () {},
+"resolve" : function () {},
+"start" : function () {},
+"type" : function () {}
+}
---
+++ app/prod/js/externs_front.js
@@ -0,0 +1,4 @@
+var TopLevel = {
+"document" : function () {},
+"getElementById" : function () {}
+}
