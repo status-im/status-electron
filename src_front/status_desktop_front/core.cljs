6df6c057e7f78a5c2b5ef45e3bd59e2ae8da4526
pablodip <pablodip@gmail.com>
2018-02-04 13:47:05 +0100

    revert src_front/status_desktop_front/core.cljs

--- src_front/status_desktop_front/core.cljs
+++ src_front/status_desktop_front/core.cljs
@@ -2,30 +2,25 @@
   (:require [reagent.core :as reagent]
             [re-frame.core :as re-frame]
             status-im.ui.screens.subs
-            ;status-desktop-front.storage
+    ;status-desktop-front.storage
             status-desktop-front.ui.screens.events
             [status-desktop-front.ui.screens.views :as views]
-            ;[cljs.nodejs :as nodejs]
+    ;[cljs.nodejs :as nodejs]
             [status-desktop-front.react-native-web :as react]
             ))
 
 (defn app-root []
+  (fn []
+    [views/main]
+    ))
 
-  (reagent/create-class
-    {
-     :component-did-mount (fn [] ())
-     :display-name "root"
-     :reagent-render views/main}))
-
-(defn mount-root [root-el]
-    (.registerComponent react/app-registry "StatusDesktop" #(reagent/reactify-component root-el)))
+(defn mount-root []
+  (.registerComponent react/app-registry "StatusDesktop" #(reagent/reactify-component app-root)))
 
 
-(defn init [root-el]
-  (mount-root root-el)
+(defn init []
+  (mount-root)
   (re-frame/dispatch-sync [:initialize-app]))
 
 (defn log [message]
   (re-frame/dispatch [:log-message message]))
-
-
